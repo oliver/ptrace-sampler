89fa5f9a1af104cbbbaf629614c3860d0a2e1cd2
Oliver Gerlich <oliver.gerlich@gmx.de>
2010-10-03 23:05:18 +0200

    ptrace-sampler: basic cleanup

--- ptrace-sampler.C
+++ ptrace-sampler.C
@@ -10,10 +10,6 @@
 #include <sys/ptrace.h>
 #include <sys/user.h>
 
-#include <vector>
-
-using std::vector;
-
 
 //
 // Sampling Functions
@@ -24,12 +20,6 @@ using std::vector;
 
 int ipoffs, spoffs, bpoffs;
 
-/*struct Frame
-{
-    void* ebp;
-    void* eip;
-};*/
-
 FILE* outFile = stderr;
 
 void CreateSample (const int pid)
@@ -39,26 +29,15 @@ void CreateSample (const int pid)
     fprintf(outFile, "%d.%06d\t", int(tv.tv_sec), int(tv.tv_usec));
 
     const int ip = ptrace(PTRACE_PEEKUSER, pid, ipoffs, 0);
-	//const int sp = ptrace(PTRACE_PEEKUSER, pid, spoffs, 0);
 	const int bp = ptrace(PTRACE_PEEKUSER, pid, bpoffs, 0);
-	//printf("ip: %8x  sp: %8x  bp: %8x\n", ip, sp, bp);
 
-    // printf("frame #0:  ip: %8x  sp: %8x  bp: %8x\n", ip, sp, bp);
     fprintf(outFile, "%08x ", ip);
-
-	/*vector<Frame> frames;
-
-	Frame f1;
-	f1.ebp = (void*)bp;
-	f1.eip = (void*)ip;
-	frames.push_back(f1);*/
 
     int oldBp = bp;
     for (int i = 1; i < 10; i++)
     {
 	    const int newBp = ptrace(PTRACE_PEEKDATA, pid, oldBp, 0);
 	    const int newIp = ptrace(PTRACE_PEEKDATA, pid, oldBp+4, 0);
-	    // printf("frame #%d:  ip: %8x  sp: %8x  bp: %8x\n", i, newIp, 0, newBp);
 	    fprintf(outFile, "%08x ", newIp);
 	    oldBp = newBp;
 	    if (newBp == 0x0 /*|| newBp == 0xFFFFFFFF*/)
@@ -114,7 +93,6 @@ int main (int argc, char* argv[])
 
     ptrace(PTRACE_CONT, pid, 0, 0);
 
-    // single-step in a loop
     int64_t numSteps = 0;
     while (true)
     {
@@ -125,22 +103,13 @@ int main (int argc, char* argv[])
             break;
         }
         
-        //usleep(1);
         usleep(100);
-        //usleep(500 * 1000);
-        //sleep(1);
 
         // interrupt child
         //printf("sending SIGTRAP...\n");
         kill(pid, SIGTRAP);
 
         int sigNo = 0;
-        /*const int pRes = ptrace(PTRACE_SINGLESTEP, pid, 0, sigNo);
-        if (pRes < 0)
-        {
-            perror("singlestep error");
-            exit(1);
-        }*/
 
         //printf("waiting for child...\n");        
         waitRes = wait(&waitStat);
@@ -158,7 +127,6 @@ int main (int argc, char* argv[])
         }
 
         //printf("child paused\n");
-        //usleep(500 * 1000);
         numSteps++;
 
         CreateSample(pid);
@@ -166,7 +134,6 @@ int main (int argc, char* argv[])
         ptrace(PTRACE_CONT, pid, 0, sigNo);
         //printf("child continued\n");
 
-        //break;
     }
     printf("exiting after %lld single-steps\n", numSteps);
 
