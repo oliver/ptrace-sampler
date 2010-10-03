e24455550dd556b68fb0ddc00f4262207921b34d
Oliver Gerlich <oliver.gerlich@gmx.de>
2010-10-03 23:53:08 +0200

    samples2calltree.py: write function call chains to calltree file

--- samples2calltree.py
+++ samples2calltree.py
@@ -29,6 +29,27 @@ def handleEvent (e):
             outFd.write("fn=%s\n" % f[2])
             outFd.write("%d 0 %d\n" % (f[4], 1))
 
+    # function calls
+    outFd.write("# function calls\n")
+    currFrame = 1
+    for f in e[1][1:]:
+        if f[3] is not None:
+            prevFrame = e[1][ currFrame-1 ]
+            
+            if prevFrame[4] is not None:
+                outFd.write("fl=%s\n" % f[3])
+                outFd.write("fn=%s\n" % f[2])
+                outFd.write("cfl=%s\n" % prevFrame[3])
+                outFd.write("cfn=%s\n" % prevFrame[2])
+                outFd.write("calls=1 %d\n" % prevFrame[4])
+                outFd.write("%d 1 0\n" % f[4])
+                
+            
+            #outFd.write("%d 0 %d\n" % (f[4], 1))
+        currFrame += 1
+
+    outFd.write("\n")
+
     #sys.exit(1)
 
 if __name__ == '__main__':
