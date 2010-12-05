d960442647278ec0a27e65b137fefbb2169bc27a
Oliver Gerlich <oliver.gerlich@gmx.de>
2010-12-05 22:05:02 +0100

    samples2calltree.py: better handling of incomplete data

--- samples2calltree.py
+++ samples2calltree.py
@@ -15,34 +15,43 @@ def handleEvent (e):
 
     # NumSamples
     for f in e[1]:
-        if f[3] is not None:
+        if f[2] is not None:
+            lineNo = f[4]
+            if lineNo is None: lineNo = 0
             outFd.write("fl=%s\n" % f[3])
             outFd.write("fn=%s\n" % f[2])
-            outFd.write("%d %d 0\n" % (f[4], 1))
+            outFd.write("%d %d 0\n" % (lineNo, 1))
             break
 
     # NumIncSamples
     outFd.write("# NumIncSamples\n")
     for f in e[1]:
-        if f[3] is not None:
+        if f[2] is not None:
+            lineNo = f[4]
+            if lineNo is None: lineNo = 0
             outFd.write("fl=%s\n" % f[3])
             outFd.write("fn=%s\n" % f[2])
-            outFd.write("%d 0 %d\n" % (f[4], 1))
+            outFd.write("%d 0 %d\n" % (lineNo, 1))
 
     # function calls
     outFd.write("# function calls\n")
     currFrame = 1
     for f in e[1][1:]:
-        if f[3] is not None:
+        if f[2] is not None:
             prevFrame = e[1][ currFrame-1 ]
             
-            if prevFrame[4] is not None:
+            if prevFrame[2] is not None:
+                lineNo = f[4]
+                if lineNo is None: lineNo = 0
+                prevLineNo = prevFrame[4]
+                if prevLineNo is None: prevLineNo = 0
+
                 outFd.write("fl=%s\n" % f[3])
                 outFd.write("fn=%s\n" % f[2])
                 outFd.write("cfl=%s\n" % prevFrame[3])
                 outFd.write("cfn=%s\n" % prevFrame[2])
-                outFd.write("calls=1 %d\n" % prevFrame[4])
-                outFd.write("%d 1 0\n" % f[4])
+                outFd.write("calls=1 %d\n" % prevLineNo)
+                outFd.write("%d 1 1\n" % lineNo)
                 
             
             #outFd.write("%d 0 %d\n" % (f[4], 1))
