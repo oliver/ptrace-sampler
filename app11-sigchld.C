1786bec73e9390dffe2ea844e34671968af5db98
Oliver Gerlich <oliver.gerlich@gmx.de>
2012-06-17 15:18:10 +0200

    app11-sigchld: create more signals for testing

--- app11-sigchld.C
+++ app11-sigchld.C
@@ -7,22 +7,34 @@
 #include <unistd.h>
 #include <signal.h>
 
+#include <sys/types.h>
+#include <sys/wait.h>
+
 
 void SigchldHandler (int sig)
 {
     printf("signal %d\n", sig);
 }
 
+void Sigusr1Handler (int sig)
+{
+    printf("Sigusr1Handler - signal %d\n", sig);
+}
+
 int main (int argc, char* argv[])
 {
     printf("main pid: %d\n", getpid());
 
-    //signal(SIGCHLD, SigchldHandler);
+    signal(SIGCHLD, SigchldHandler);
+    signal(SIGUSR1, Sigusr1Handler);
 
+    int counter = 0;
     while (true)
     {
-        printf("looping...\n");
+        counter++;
+        printf("iteration #%d...\n", counter);
 
+        kill(getpid(), SIGUSR1);
         const int pid = fork();
         if (pid == 0)
         {
@@ -30,6 +42,19 @@ int main (int argc, char* argv[])
             sleep(1);
             exit(0);
         }
+
+        const int pid2 = fork();
+        if (pid2 == 0)
+        {
+            exit(0);
+        }
+
+        int waitRes;
+        do
+        {
+            int waitStat;
+            waitRes = waitpid(-1, &waitStat, WNOHANG);
+        } while (waitRes > 0);
 
         sleep(2);
     }
